5e59016839aaddc52c79433affd9a32b407d62b7
Louis R. <roccalouis26@gmail.com>
2023-10-19 16:45:35 +0200

    vercel deployment

--- src/page/calendar/Calendar.tsx
+++ src/page/calendar/Calendar.tsx
@@ -1,22 +1,22 @@
 import { useState } from "react";
 import s from "./Calendar.module.scss";
-import { useDispatch, useSelector } from "react-redux";
-import { RootState } from "../../store/store";
-import { setTitle } from "../../reducer/CalendarTaskReducer";
+// import { useDispatch, useSelector } from "react-redux";
+// import { RootState } from "../../store/store";
+// import { setTitle } from "../../reducer/CalendarTaskReducer";
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import SideBarCalendar from "../../component/sidebarCalendar/SideBarCalendar";
 import SchedularCalendar from "../../component/schedulerCalendar/SchedulerCalendar";
 
 export default function Calendar() {
-  const [calendarTitle, setCalendarTitle] = useState("");
+  const [calendarTitle] = useState("");
   console.log(calendarTitle);
-  const calendar = useSelector((state: RootState) => state.CalendarTaskReducer);
-  const dispatch = useDispatch();
+  // const calendar = useSelector((state: RootState) => state.CalendarTaskReducer);
+  // const dispatch = useDispatch();
 
-  const handleSetTitle = () => {
-    dispatch(setTitle(calendarTitle));
-  };
+  // const handleSetTitle = () => {
+  //   dispatch(setTitle(calendarTitle));
+  // };
 
   return (
     <section className={s.calendarPage}>
